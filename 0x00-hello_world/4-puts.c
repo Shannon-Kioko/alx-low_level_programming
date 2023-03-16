9ebd9a56d9c205856527b48569c0dd22e796ba15
Shannon-Kioko <84199763+Shannon-Kioko@users.noreply.github.com>
2023-03-17 01:38:49 +0300

    Add files via upload

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char myStr[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(myStr);
+	return (0);
+}
