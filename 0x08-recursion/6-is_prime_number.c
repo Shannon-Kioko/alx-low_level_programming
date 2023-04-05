2b9cfed7fcd0c4444104badba902bb1b13776d75
Shannon-Kioko <shannonkaluki@gmail.com>
2023-04-05 02:50:21 -0700

    Adding file to check for prime numbers

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * is_prime_number - determines if a given number is prime
+ *
+ * @n: input integer
+ *
+ * Return: 1 if prime, 0 otherwise
+ */
+
+int is_prime_number(int n)
+{
+	int div = 2;
+
+	if (n <= 1)
+		return (0);
+
+	if (n <= 3)
+		return (1);
+
+	return (is_divisible(n, div));
+}
+
+/**
+ * is_divisible - determines if a given number is divisible by a divisor
+ *
+ * @num: input integer
+ * @div: divisor
+ *
+ * Return: 1 if divisible, 0 otherwise
+ */
+
+int is_divisible(int num, int div)
+{
+	if (num % div == 0)
+		return (0);
+
+	if (div == num / 2)
+		return (1);
+
+	return (is_divisible(num, div + 1));
+}
+
