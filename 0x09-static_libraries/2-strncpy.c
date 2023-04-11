e9c0e2521054344ecdff4aefb7c72df4ffd0a345
Shannon-Kioko <84199763+Shannon-Kioko@users.noreply.github.com>
2023-04-11 04:39:25 +0300

    Update 2-strncpy.c

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed to by src to dest
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+  return(0);
+}
