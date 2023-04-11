449fc3890ac1cffd3f1cb5440737193ee66da967
Shannon-Kioko <84199763+Shannon-Kioko@users.noreply.github.com>
2023-04-11 04:42:13 +0300

    Update 1-strncat.c

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,5 +1,15 @@
 #include "main.h"
 
+/**
+ * _strncat - concatenates two strings, with n as limit
+ * @dest: destination string
+ * @src: source string
+ * @n: maximum number of bytes from src
+ *
+ * Return: pointer to resulting string dest
+ */
+
 char *_strncat(char *dest, char *src, int n)
 {
+  return (0);
 }
