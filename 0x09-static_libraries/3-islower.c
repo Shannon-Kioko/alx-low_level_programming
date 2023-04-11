b67e0006b041a969556ad1e38c32c28b21056b69
Shannon-Kioko <84199763+Shannon-Kioko@users.noreply.github.com>
2023-04-11 04:27:16 +0300

    Update 3-islower.c

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - check for lowercase character
+ *
+ * @c: the char to be checked
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+}
