2a2d8d0ccc6751bc26b76a7eb1eaedf9c218577c
Shannon-Kioko <84199763+Shannon-Kioko@users.noreply.github.com>
2023-04-11 04:33:27 +0300

    Update 2-strlen.c

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,12 @@
+/**
+ * _strlen - returns the length of a string
+ * @s: The string to be measured
+ *
+ * Return: The length of s
+ */
+
+int _strlen(char *s)
+{
+	/* code goes here */
+	return (0);
+}
