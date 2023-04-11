5e99ef387da9f3ced673ce8246bc4c323e53c406
Shannon-Kioko <shannonkaluki@gmail.com>
2023-04-10 17:21:44 -0700

    Updating 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,10 +6,10 @@
  * @argv: Array of strings containing the arguments
  * Return: Always 0 (Success)
  */
-int main(int argc,char*argv[])
+int main(int argc, char*argv[])
 {
-	if(argc>0)
-		printf("%s\n",argv[0]);
+	if(argc > 0)
+		printf("%s\n", argv[0]);
 	return(0);
 }
 
