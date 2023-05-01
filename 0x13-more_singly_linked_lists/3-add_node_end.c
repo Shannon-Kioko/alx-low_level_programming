fca86d6ad9b0014c95a173938c3fd88fa96f2ce1
Shannon-Kioko <84199763+Shannon-Kioko@users.noreply.github.com>
2023-05-01 13:05:33 +0300

    Update 3-add_node_end.c

--- 0x13-more_singly_linked_lists/3-add_node_end.c
+++ 0x13-more_singly_linked_lists/3-add_node_end.c
@@ -14,7 +14,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
-		return NULL;
+		return (NULL);
 
 	new_node->n = n;
 	if (*head == NULL)
@@ -28,8 +28,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		tmp = *head;
 		while (tmp->next)
 			tmp = tmp->next;
-    
+
 		tmp->next = new_node;
 	}
-	return new_node;
+	return (new_node);
 }
