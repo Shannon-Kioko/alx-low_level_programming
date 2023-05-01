6d3582d7e7057e0154ee0ee2fc7a115034825d71
Shannon-Kioko <84199763+Shannon-Kioko@users.noreply.github.com>
2023-05-01 11:26:01 +0300

    Create 1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+/*
+ * This returns the numof elements in a linked list.
+ *
+ * @h: pointer to the list struct.
+ *
+ * Return: numof elements in the linked list.
+ */
+#include "lists.h"
+
+size_t listint_len(const listint_t *h)
+{
+	size_t numo_elements = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		numo_elements++;
+	}
+	return numo_elements;
+}
