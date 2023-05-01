e8f6664ca67fc3b9ce4ccf30781e247611b6b633
Shannon-Kioko <84199763+Shannon-Kioko@users.noreply.github.com>
2023-05-01 14:00:00 +0300

    Update 4-isalpha.c

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -7,6 +7,20 @@
  *
  * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
  */
+ 
 int _isalpha(int c)
 {
+  char u, l;
+	int isletter = 0;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		for (u = 'A'; u <= 'Z'; u++)
+		{
+			if (c == l || c == u)
+				isletter = 1;
+		}
+	}
+	return (isletter);
 }
+  
