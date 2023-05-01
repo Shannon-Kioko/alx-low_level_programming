79c2a7758bdae79bd629725468ebc4b732c28c78
Shannon-Kioko <84199763+Shannon-Kioko@users.noreply.github.com>
2023-05-01 12:59:42 +0300

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -27,6 +27,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index);
  * Description: function listint_len returns the number of
  * elements in a linked list.
  * starting from given head node.
+ *
  * Return: numof elements in the linked list.
  */
 size_t listint_len(const listint_t *h);
