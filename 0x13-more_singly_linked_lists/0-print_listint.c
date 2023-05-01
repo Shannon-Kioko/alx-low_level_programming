227347972db4bfd7c6ff0f61ca441cb0f4afcb3c
Shannon-Kioko <84199763+Shannon-Kioko@users.noreply.github.com>
2023-05-01 11:23:38 +0300

    Create 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+/* 
+ * print_listint - This prints all the elements of a singly linked list
+ * 
+ * @h: pointer to the list struct.
+ * 
+ * Return: number of nodes in the list.
+ */
+#include "lists.h"
+
+size_t print_listint(const listint_t *h)
+{
+	size_t num_nodes = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		num_nodes++;
+	}
+	return num_nodes;
+}
