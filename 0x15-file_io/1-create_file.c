3a48e30e99b2fe17302cb25f93b2f1317b00659b
Shannon-Kioko <84199763+Shannon-Kioko@users.noreply.github.com>
2023-05-10 03:13:26 +0300

    Create 1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file with the filename and writes the
+ * text_content to it
+ * @filename: the name of the file to create
+ * @text_content: the text content to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, return_val, txt_len = 0;
+
+	if (!filename)
+  {
+    return (-1);
+  }
+  
+	if (text_content)
+	{
+		while (text_content[txt_len])
+			txt_len++;
+	}
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		return_val = write(fd, text_content, txt_len);
+		if (return_val == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+
+	return (1);
+}
