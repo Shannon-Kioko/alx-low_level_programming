76f6af5f5b1d500dae444f0db97f93fb26125655
Shannon-Kioko <84199763+Shannon-Kioko@users.noreply.github.com>
2023-05-10 03:48:07 +0300

    Update main.h betty block comments

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,8 +6,9 @@
 #include <fcntl.h>
 #include <stdio.h>
 
-/* read_textfile - function that reads a text file and prints it to
- *the POSIX standard output.
+/*
+ * read_textfile - function that reads a text file and prints it to
+ * the POSIX standard output.
  */
 ssize_t read_textfile(const char *filename, size_t letters);
 
@@ -23,9 +24,10 @@ int copy_file(const char *file_from, const char *file_to);
 /* print_error - prints error message to stderr */
 void print_error(int code, const char *file);
 
-/* print_elf_info - prints information contained in the ELF header 
-* at the start of an ELF file.
-*/
+/*
+ * print_elf_info - prints information contained in the ELF header 
+ * at the start of an ELF file.
+ */
 void print_elf_info(unsigned char *header);
 
 #endif /* MAIN_H */
