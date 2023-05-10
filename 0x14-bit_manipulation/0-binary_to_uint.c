1a02ae0bd991461c4dde41061cd3f36fd57dbd69
Shannon-Kioko <84199763+Shannon-Kioko@users.noreply.github.com>
2023-05-10 04:03:07 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,7 +12,7 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int result = 0;
 	int len, power = 0;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
 
 	for (len = strlen(b) - 1; len >= 0; len--)
