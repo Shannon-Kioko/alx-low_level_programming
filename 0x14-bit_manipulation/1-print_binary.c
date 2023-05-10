d0204752e6180f751306c7f195aac02d06e9f62c
Shannon-Kioko <84199763+Shannon-Kioko@users.noreply.github.com>
2023-05-10 04:08:25 +0300

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,8 +9,16 @@
 
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
-		print_binary(n >> 1);
+	unsigned long int mask = 1;
+	mask <<= (sizeof(unsigned long int) * 8) - 1;
+	
+	while (mask)
+	{
+		if (n & mask)
+			_putchar('1');
+		else
+			_putchar('0');
 
-	printf("%d", (int)(n & 1));
+		mask >>= 1;
+	}
 }
