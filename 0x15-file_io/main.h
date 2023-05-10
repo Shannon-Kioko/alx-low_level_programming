56b8c24d0136e4aff3664d8799f2ff9216940d14
Shannon-Kioko <84199763+Shannon-Kioko@users.noreply.github.com>
2023-05-10 03:43:31 +0300

    betty block comments
     use a leading /* on a separate line

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,7 +6,8 @@
 #include <fcntl.h>
 #include <stdio.h>
 
-/* read_textfile - function that reads a text file and prints it to the POSIX standard output. */
+/* read_textfile - function that reads a text file and prints it to
+the POSIX standard output. */
 ssize_t read_textfile(const char *filename, size_t letters);
 
 /* create_file - function that creates a file. */
@@ -21,7 +22,7 @@ int copy_file(const char *file_from, const char *file_to);
 /* print_error - prints error message to stderr */
 void print_error(int code, const char *file);
 
-/** print_elf_info - prints information contained in the ELF header 
+/* print_elf_info - prints information contained in the ELF header 
 *at the start of an ELF file.
 */
 void print_elf_info(unsigned char *header);
