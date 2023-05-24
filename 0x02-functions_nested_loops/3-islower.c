017767831ba8e0a7b53b359696d5f7842bd08711
Shannon-Kioko <shannonkaluki@gmail.com>
2023-05-24 10:44:33 -0700

    Adding FIles

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -20,3 +20,18 @@ int _islower(int c)
 	}
 	return (low);
 }
+
+int main(void)
+{
+    int r;
+
+    r = _islower('H');
+    _putchar(r + '0');
+    r = _islower('o');
+    _putchar(r + '0');
+    r = _islower(108);
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -15,5 +15,18 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+
 #endif
 
