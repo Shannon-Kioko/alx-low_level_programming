a6fb751cf7eff9e5038f9828d73dfae9be3dfc3e
Shannon-Kioko <shannonkaluki@gmail.com>
2023-06-15 13:53:01 -0700

    Move files to current directory

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - Returns the length of a doubly linked list.
+ *
+ * @h: Pointer to the head of the list.
+ *
+ * Return: The number of nodes in the list.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t node_count = 0;
+
+	while (h)
+	{
+		node_count++;
+		h = h->next;
+	}
+
+	return (node_count);
+}
