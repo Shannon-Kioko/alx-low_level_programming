d0c21a5a94d321c0fe684ce6639c7bc7b2bc930b
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2023-07-06 23:22:07 +0300

    Update 1-a-main.c

--- 0x1A-hash_tables/1-a-main.c
+++ 0x1A-hash_tables/1-a-main.c
@@ -5,13 +5,8 @@
  */
 int main(void)
 {
-    char *s;
+    const char *s = "Hello, world!";
+    printf("%lu\n", hash_djb2((const unsigned char *)s));
 
-    s = "cisfun";
-    printf("%lu\n", hash_djb2((unsigned char *)s));
-    s = "Don't forget to tweet today";
-    printf("%lu\n", hash_djb2((unsigned char *)s));
-    s = "98";
-    printf("%lu\n", hash_djb2((unsigned char *)s));
-    return (EXIT_SUCCESS);
+    return 0;
 }
