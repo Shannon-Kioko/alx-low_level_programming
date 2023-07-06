520873824d1b7be15f8045a56290b0db2634c800
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2023-07-06 23:49:31 +0300

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,17 +9,17 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	char *key_copy = strdup(key);
+	
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
-
-	unsigned long int index = key_index((const unsigned char *)key, ht->size);
 
 	hash_node_t *new_node = malloc(sizeof(hash_node_t));
 
 	if (new_node == NULL)
 		return (0);
 
-	char *key_copy = strdup(key);
 	char *value_copy = (value != NULL) ? strdup(value) : NULL;
 
 	if (value != NULL && value_copy == NULL)
