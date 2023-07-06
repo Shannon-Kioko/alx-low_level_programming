229910cb78fb6284270aa0867206695f08d2daf6
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2023-07-07 01:29:22 +0300

    betty fixes -> description

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -77,6 +77,7 @@ typedef struct shash_table_s
  * Return -  pointer to the newly created hash table
  **/
 shash_table_t *shash_table_create(unsigned long int size);
+
 /** 
  * shash_table_set - adds an element to the hash table.
  * @ht - hash table you want to add or update the key/value to
@@ -86,6 +87,7 @@ shash_table_t *shash_table_create(unsigned long int size);
  * Return - 1 if success, 0 otherwise
 **/
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
+
 /**
 * shash_table get - retrieves a value associated with a key.
 * @ht - the hash table you want to look into
@@ -93,6 +95,7 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value);
 * Return - value if success, otherwise NULL 
 **/
 char *shash_table_get(const shash_table_t *ht, const char *key);
+
 void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
