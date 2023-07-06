ee1439fbbfff37a93216683c079aab97c54ea0c8
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2023-07-06 23:36:35 +0300

    Update 2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,9 +1,9 @@
 #include "hash_tables.h"
 /**
  * key_index - Function that gives the index of a key
- * key - key to get index for
- * size - unint long for size of array of hash table
- * returns - index the key/val pair should be
+ * @key - key to get index for
+ * @size - unint long for size of array of hash table
+ * Return - index the key/val pair should be
  * stored in the arr of hash table
 **/
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
