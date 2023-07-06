ea9313947858ef3f4eca528980fbe0ae4573e7ad
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2023-07-07 00:25:20 +0300

    Add  6-hash_table_delete.c

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,29 @@
+/**
+ * hash_table_delete - Deletes a hash table
+ * @ht: The hash table to delete
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *node;
+	hash_node_t *current;
+
+	if (ht == NULL)
+		return;
+
+	unsigned long int i;
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			current = node;
+			node = node->next;
+			free(current->key);
+			free(current->value);
+			free(current);
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
